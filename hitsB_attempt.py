be61e7d7e091769682345c2ac2bb85059f01f536
nazeez <42068453+nazeez@users.noreply.github.com>
2019-01-19 22:53:48 -0500

    Attempt at two baseline features
    Need to modify them to calculate the feature value for each table

---
+++ hitsB_attempt.py
@@ -0,0 +1,60 @@
+import errno
+import glob  # glob module finds all the path names matching a specified pattern
+
+path = '/Users/naila/Desktop/WP_tables/tables_redi2_1/*.json'
+files = glob.glob(path)
+for name in files:
+    try:
+        with open(name) as f:  # opens the files and read them
+            corpus = f.read()
+
+            def tokenize():
+                if corpus is not None:
+                    words = corpus.lower().split()
+                    return words
+                else:
+                    return None
+
+            def map_table(tokens):
+                hash_map = {}
+
+                if tokens is not None:
+                    for element in tokens:
+                        word = element.replace(",", "")
+                        word = word.replace(".", "")
+
+                        if word in hash_map:
+                            hash_map[word] = hash_map[word] + 1
+                        else:
+                            hash_map[word] = 1
+                    return hash_map
+                else:
+                    return None
+
+            words = tokenize()
+            word_list = ['title', 'me']
+
+            map = map_table(words)
+
+            for word in word_list:
+                print('Word: [' + word + '] Frequency: ' + str(map[word]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
