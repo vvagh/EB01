05c1bcbaff8c6fdd1b2521a6cfd1ca27e3cc819e
nazeez <42068453+nazeez@users.noreply.github.com>
2019-01-23 15:32:37 -0500

    Progressing Code: Writing to CSV File and features
    Includes the headings for the table and the feature values for number of rows and number of columns

---
+++ Features/NumRows_NumColumns.py
@@ -0,0 +1,29 @@
+import json
+import csv
+from itertools import zip_longest
+
+sourceFile = open('Corpus/query_1.json', 'r')
+json_data = json.load(sourceFile)
+
+# number of data rows
+tR_0370 = json_data["table-0370-614"]["numDataRows"]
+# number of columns
+tC_0370 = json_data["table-0370-614"]["numCols"]
+
+with open('Features.csv', 'w', encoding="ISO-8859-1", newline='') as myFile:
+    # writer for csv file
+    wr = csv.writer(myFile)
+    # creating the headings for csv file
+    wr.writerow(("query_id", "query", "table_id", "row", "col", "nul", "in_link", "out_link", "pgCount", "tImp", "tPF",
+                 "leftColhits", "secColHits", "bodyHits", "PMI", "qInPgTitle", "qInTableTitle", "yRank", "csr_score",
+                 "idf"))
+
+    query_id = ['1'] * 20
+    query = ['world interest rates table'] * 20
+    table_id = ["table-0370-614"]
+    numRows = [tR_0370]
+    numCols = [tC_0370]
+    d = [query_id, query, table_id, numRows, numCols]
+    exported_data = zip_longest(*d, fillvalue='')
+    wr.writerows(exported_data)
+myFile.close()
