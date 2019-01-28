9938ce0200ee0148d5c143796428513ebdfae4da
nazeez <42068453+nazeez@users.noreply.github.com>
2019-01-27 21:56:31 -0500

    Simplified version of # rows and columns feature

--- Features/NumRows_NumColumns.py
+++ Features/NumRows_NumColumns.py
@@ -1,38 +1,21 @@
 import json
-import csv
-from itertools import zip_longest
 
-sourceFile = open('Corpus/query_1.json', 'r')
+sourceFile = open('corpus.json', 'r')
 json_data = json.load(sourceFile)
 
-# number of data rows
-tR_0370 = json_data["table-0370-614"]["numDataRows"]
-# number of columns
-tC_0370 = json_data["table-0370-614"]["numCols"]
 
-# Counts number of null cells in Data portions of table
-counting = []
-for entry in json_data["table-0370-614"]["data"]:
-    counting.append(entry.count(""))
-    # count = entry.count("")
-    sum_result = sum(counting)
-print(sum_result)
+fname = 'STR.txt'
 
-with open('Features.csv', 'w', encoding="ISO-8859-1", newline='') as myFile:
-    # writer for csv file
-    wr = csv.writer(myFile)
-    # creating the headings for csv file
-    wr.writerow(("query_id", "query", "table_id", "row", "col", "nul", "in_link", "out_link", "pgCount", "tImp", "tPF",
-                 "leftColhits", "secColHits", "bodyHits", "PMI", "qInPgTitle", "qInTableTitle", "yRank", "csr_score",
-                 "idf"))
-
-    query_id = ['1'] * 20
-    query = ['world interest rates table'] * 20
-    table_id = ["table-0370-614"]
-    numRows = [tR_0370]
-    numCols = [tC_0370]
-    nullCells = [sum_results]
-    d = [query_id, query, table_id, numRows, numCols, nullCells]
-    exported_data = zip_longest(*d, fillvalue='')
-    wr.writerows(exported_data)
-myFile.close()
+with open(fname) as f:
+    content = f.readlines()
+rows = []
+columns = []
+for line in content:
+    sline = line.split()
+    tid = sline[2]
+    # number of data rows
+    tR = json_data[tid]["numDataRows"]
+    # number of data columns
+    tC = json_data[tid]["numCols"]
+    rows.append(tR)
+    columns.append(tC)
