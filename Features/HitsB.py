57cb09d6052e03207151bebcb4a3827d2f0d3f26
Vruti Vaghela <vruti@Vrutis-MacBook-Pro-2.local>
2019-02-02 17:43:27 -0500

    Update HitsB.py

--- Features/HitsB.py
+++ Features/HitsB.py
@@ -1,12 +1,14 @@
-
 # This file represents the #hitsB which is the total query term frequency in the table body
 # this json file demonstrates a test run and the output per work and character
 import json
+import QT_ID
 
-sourceFile = open('/Users/fatimapeygumbari/EB01/Corpus_Data/corpus.json', 'r')
-json_data = json.load(sourceFile)
 
-fname = '/Users/fatimapeygumbari/EB01/Corpus_Data/STR.txt'
+sourceFile = open('corpus.json', 'r')
+jsonData = json.load(sourceFile)
+json_data = jsonData.replace("_", " ")
+
+fname = 'STR.txt'
 
 with open(fname) as f:
         content = f.readlines()
@@ -18,10 +20,12 @@ for line in content:
         # Counts number of null cells in Data portions of table
         counting = []
         sumResults = []
-        for entry in json_data[tid]["data"]:
-                counting.append(entry.count("World"))
-                sum_result = sum(counting)
-                sumResults = sum_result
-        query_words.append(sum_result)
+        for entry in json_data[tid]["data"] or json_data[tid]["pgTitle"] or json_data[tid]["title"] or json_data[tid]["caption"] or json_data[tid]["secondTitle"]:
+            query = QT_ID.query_list[0].split(' ')
+            # print(query[2])
 
+            counting.append(entry.count("capita"))
+            sum_result = sum(counting)
+            sumResults = sum_result
+        query_words.append(sum_result)
 print(query_words)
